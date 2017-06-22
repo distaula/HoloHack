dfd6c55f637b9292e132fb2ea9c59a18ec39c7dc
Joe Marchuk <j.marchuk3@gmail.com>
2017-06-22 11:30:02 -0400

    SFX and empty object for music later

---
+++ Assets/OurAssets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+	public GameObject player;
+	Vector3 playerPos;
+	// Use this for initialization
+	void Start () {
+		playerPos = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = playerPos;
+		playerPos = player.transform.position;
+	}
+}
